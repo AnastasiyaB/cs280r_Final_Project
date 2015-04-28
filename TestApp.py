ea616e12c814809f11ec38bb565cadf9f0154743
Serguei Balanovich <sbalanovich@college.harvard.edu>
2015-04-28 13:02:06 -0400

    presentation demo

--- TestApp.py
+++ TestApp.py
@@ -8,13 +8,15 @@ from kivy.uix.slider import Slider
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.gridlayout import GridLayout
 from kivy.uix.image import Image
+from kivy.uix.button import Button
+from kivy.uix.textinput import TextInput
 from kivy.graphics import *
 from kivy.clock import Clock
 from kivy.lang import Builder
 
 import firesim
 
-class FireUI(BoxLayout):
+class FireUI(GridLayout):
     pass
 
 class FireModel(GridLayout):
@@ -22,6 +24,13 @@ class FireModel(GridLayout):
 
 class FireFighters(GridLayout):
     pass
+
+class InputPanel(BoxLayout):
+    def __init__(self, size_hint, **kwargs):
+        super(InputPanel, self).__init__(**kwargs)
+        self.orientation = 'vertical'
+        self.size_hint = size_hint
+        self.spacing = 3
 
 class Fighter(Widget):
     def __init__(self, x, y, effectiveness, **kwargs):
@@ -66,8 +75,10 @@ class CustomLayout(Widget):
         i = self.intensity
         if self.cellType == "fuel":
             rgb = (0, i, 0)
-        else:
+        elif self.cellType == "fire":
             rgb = (1, i, 0)
+        else:
+            rgb = (0, 0, 1)
         return rgb
 
     def update(self, intensity):
@@ -84,21 +95,44 @@ class FireApp(App):
         self.size = size
         self.slabel = Label(text='0', size_hint=(1, .05))
         self.model = None
+        self.slider = None
         self.vals = []
         self.iters = iters
         self.currIter = 0
+        self.inputs = {}
 
     def OnSliderValueChange(self, instance, value):
         self.slabel.text = str(value)
         self.currIter = int(value)
         self.updateCells()
 
-    def generateSim(self):
+    def generateSim(self, value=0):
+        self.currIter = 0
+        self.vals = []
         sim = firesim.AreaSimulation(self.cols)
         sim.initialize()
+        if self.inputs:
+            for ipt in self.inputs["Fire"]:
+                sim.grid[(int(ipt[0].text), int(ipt[1].text))].fire_inten = float(ipt[2].text)
+                print int(ipt[0].text), int(ipt[1].text)
+            for ipt in self.inputs["FF"]:
+                if int(ipt[2].text):
+                    ff = firesim.FireFighter(int(ipt[0].text), int(ipt[1].text), sim, efficacy = int(ipt[2].text))
+                    sim.fight_fire(ff)
+        else:
+            ### now I'm going to "start" a fire in the upper left corner
+            sim.grid[(3,2)].fire_inten = .55
+            sim.grid[(1,1)].fire_inten = .60
+            sim.grid[(2,1)].fire_inten = .31
+            
         for i in range(self.iters):
             self.vals.append(sim.grid)
             sim.gnew()
+
+        if self.model:
+            self.updateCells()
+        if self.slider:
+            self.slider.value = 0
 
     def updateCells(self):
         if self.model and self.model.children:
@@ -107,14 +141,13 @@ class FireApp(App):
         for x in range(self.cols):
             for y in range(self.cols):
                 if self.vals:
-                    fire_inten = self.vals[self.currIter][(x, y)].fire_inten
-                    if x == 0 and y == 0:
-                        print self.vals[self.currIter][(x, y)].fire_inten
-                        #self.grid[(1,2)].fire_inten = .55
-
-                    ct = "fire" if fire_inten else "fuel"
-                    it = fire_inten if fire_inten else self.vals[self.currIter][(x, y)].veg_inten
-                    c = CustomLayout(self.cols, x, y, subSize, cellType = ct, intensity = it)
+                    if self.vals[self.currIter][(x, y)].firefighter:
+                        c = CustomLayout(self.cols, x, y, subSize, cellType = "ff")
+                    else:
+                        fire_inten = self.vals[self.currIter][(x, y)].fire_inten
+                        ct = "fire" if fire_inten else "fuel"
+                        it = fire_inten if fire_inten else self.vals[self.currIter][(x, y)].veg_inten
+                        c = CustomLayout(self.cols, x, y, subSize, cellType = ct, intensity = it)
                 else:
                     c = CustomLayout(self.cols, x, y, subSize)
                 self.model.add_widget(c)
@@ -122,20 +155,41 @@ class FireApp(App):
     def build(self):
         self.generateSim()
         
-        root = FireUI(orientation='vertical', size=(800, 1000))
+        root = FireUI(cols=2, size=(800, 1000))
         
-        self.model = FireModel(cols = self.cols, size_hint=(.8, .8))
+        self.model = FireModel(cols = self.cols, size_hint=(.7, .7))
         self.updateCells()
 
-        s = Slider(max = self.iters-1, step = 1, size_hint=(1, .15))
-        s.bind(value = self.OnSliderValueChange)
+        self.slider = Slider(max = self.iters-1, step = 1, size_hint=(1, .15))
+        self.slider.bind(value = self.OnSliderValueChange)
+
+        panel = InputPanel(size_hint=(.27, .8))
+        deflt = {"Fire": [(1, 2, .55), (1, 1, .6), (2, 1, .31)], "FF": [(3, 2, 0), (4, 5, 0), (5, 1, 0)]}
+        inputs = {'Fire': [], 'FF': []}
+        for t in ['Fire', 'FF']:
+            for i in range(1, 4):
+                panel.add_widget(Label(text = str(t) + " " + str(i) + ' Coords: x, y, int', size_hint=(1, .15)))
+                inpt = GridLayout(cols = 3, size_hint = (1, .15))
+                tup = ()
+                for j in range(3):
+                    txt = TextInput(text=str(deflt[t][i-1][j]), multiline = False)
+                    tup += (txt,)
+                    inpt.add_widget(txt)
+                panel.add_widget(inpt)
+                inputs[t].append(tup)
+        self.inputs = inputs
+        
+        genBtn = Button(text="Generate!!", size_hint=(1, .15))
+        panel.add_widget(genBtn)
+        genBtn.bind(on_press = self.generateSim)
         
         root.add_widget(self.model)
-        root.add_widget(s)
-        root.add_widget(self.slabel)
+        root.add_widget(panel)
+        root.add_widget(self.slider)
+        #root.add_widget(self.slabel)
         #Clock.schedule_interval(game.update, 1.0 / 60.0)
         return root
 
 
 if __name__ == '__main__':
-    FireApp(30, 300).run()
+    FireApp(90, 900).run()
--- firesim.py
+++ firesim.py
@@ -2,11 +2,12 @@ import random
 
 ## class representation for each cell
 class Cell:
-    def __init__(self, x, y, time, veg_inten, wind_direc, wind_inten, fire_inten,firefighter, ff_info):
+    def __init__(self, x, y, time, veg_inten, wind_direc, wind_inten, fire_inten, firefighter, ff_info):
         self.x = x
         self.y = y
         self.time = time
         self.veg_inten = veg_inten # vegetation intensity (assuming the most intense, the more flammable; 0 means it can no longer catch on fire)
+        #self.veg_vol = veg_vol #vegetation volume
         self.wind_direc = wind_direc # wind direction
         self.wind_inten = wind_inten # wind intensity
         self.fire_inten = fire_inten # if there is a fire in the cell, how intensly is it burning
@@ -15,16 +16,43 @@ class Cell:
         
 ## class representation for each firefighter
 class FireFighter:
-    def __init__(self, cell, efficacy = 1):
-        self.cell = cell # The starting cell/cell we're currently in
+    def __init__(self, x, y, area, efficacy = 1):
+        self.x = x # The starting cell/cell we're currently in
+        self.y = y
+        self.area = area # The grid where the firefighter lives
         self.path = [] # The path we've traverse so far
         self.efficacy = efficacy # Perhaps make this model vary on fighter skill level
         self.actList = range(8) # 0 = up left, ... 7 = left
 
-    def bestAction(self):
-        actList = [(-1,0),(0,-1),(0,1),(1,0)]
-        act = random.choice(actList)
+    def bestAction(self, newgrid):
+        #actList = [(-1,0),(0,-1),(0,1),(1,0)]
+        actList = [(-1,-1), (-1,0), (-1,1), (0,-1), \
+                   (0, 1), (1,-1),  (1,0),  (1,1)]
+        # Greedy
+        max_inten = 0.
+        max_move = None
+        for dx, dy in actList:
+            if self.x + dx >= 0 and self.x + dx < self.area.L \
+               and self.y + dy >= 0 and self.y + dy < self.area.L:
+                it = self.area.grid[(self.x + dx, self.y + dy)].fire_inten
+                fft = newgrid[(self.x + dx, self.y + dy)].firefighter
+                if it > max_inten and not fft:
+                    max_inten = it
+                    max_move = (dx, dy)
+        act = max_move
+        while not act:
+            # Random
+            act = random.choice(actList)
+            dx, dy = act
+            if self.x + dx >= 0 and self.x + dx < self.area.L \
+               and self.y + dy >= 0 and self.y + dy < self.area.L \
+               and not newgrid[(self.x + dx, self.y + dy)].firefighter:
+                break
+            else:
+                act = None
         self.path.append(act)
+        self.x += act[0]
+        self.y += act[1]
         return act
        
 class AreaSimulation:
@@ -36,10 +64,12 @@ class AreaSimulation:
         
         self.L = L
         self.time = 0
+        self.firefighters = [[False for i in range(L)] for j in range(L)]
         
-    def fight_fire(self,x,y,ff_info):
-        self.grid[(x,y)].firefighter = True
-        self.grid[(x,y)].ff_info = ff_info
+    def fight_fire(self, ff_info):
+        self.grid[(ff_info.x, ff_info.y)].firefighter = True
+        self.grid[(ff_info.x, ff_info.y)].ff_info = ff_info
+        self.firefighters[ff_info.x][ff_info.y] = True
         return
         
     ## rewrite this
@@ -51,16 +81,13 @@ class AreaSimulation:
                     y= y,
                     time= 0,
                     veg_inten= random.random(),
+                    #veg_vol = random.random()*10,
                     wind_direc= "right", ### not using this for now
                     wind_inten= random.random(),
                     fire_inten= 0.00,
                     firefighter = False,
                     ff_info = None 
                 )
-        ### now I'm going to "start" a fire in the upper left corner
-        self.grid[(1,2)].fire_inten = .55
-        self.grid[(1,1)].fire_inten = .60
-        self.grid[(2,1)].fire_inten = .31
         
         return self.grid
     
@@ -75,36 +102,15 @@ class AreaSimulation:
 #                     newgrid[(x,y)] = self.grid[(x,y)] ## need to update time
 #                     newgrid[(x,y)].time +=1
 #                     newgrid[(x,y)].fire_inten = 0
-                if self.grid[(x,y)].firefighter == True: ## there is a firefighter in that cell
-                    print "fire fighter coord", x,y
-                    newgrid[(x,y)] = Cell(
-                        x= x,
-                        y= y,
-                        time = self.time,
-                        veg_inten= self.grid[(x,y)].veg_inten, # vegetation preserved
-                        wind_direc= self.grid[(x,y)].wind_direc,
-                        wind_inten= self.grid[(x,y)].wind_inten,
-                        fire_inten= 0.0, # fire put out
-                        firefighter = False, ### since firefighter leaves the cell
-                        ff_info = None 
-                   )
-                    # append that we have traversed 
-                    self.grid[(x,y)].ff_info.path.append((x,y))
-                    ## pick where the fire firefighter will be in the next step
-#                     action = self.grid[(x,y)].ff_info.bestAction() ## somewhere here need to check if it's a legal action
-                    
-#                     # there should be a way to get this at the end
-#                     newgrid[(x+action[0], y+action[1])].firefighter = True
-#                     newgrid[(x+action[0], y+action[1])].ff_info = self.grid[(x,y)].ff_info
-                    new_ff_coord.append((x,y))
                 ## TO DO: indicate here which cell the firefigher will go to next
-                elif self.grid[(x,y)].fire_inten > 0: # cell is burning
-                    new_fire_inten = min(self.grid[(x,y)].fire_inten *self.grid[(x,y)].veg_inten * 3,1) ### take into account vegetation
+                if self.grid[(x,y)].fire_inten > 0: # cell is burning
+                    new_fire_inten = min(self.grid[(x,y)].fire_inten * (self.grid[(x,y)].veg_inten) * 3, 1) ### take into account vegetation
                     newgrid[(x,y)] = Cell(
                         x= x,
                         y= y,
                         time = self.time if not self.grid[(x,y)].time else self.grid[(x,y)].time,
-                        veg_inten= max(0,self.grid[(x,y)].veg_inten -.01), ## figure out a better way to decay
+                        veg_inten = max(0, self.grid[(x,y)].veg_inten - .005),
+                        #veg_vol = max(0.,self.grid[(x,y)].veg_vol - (new_fire_inten/4.)),
                         wind_direc= self.grid[(x,y)].wind_direc,
                         wind_inten= self.grid[(x,y)].wind_inten,
                         # some previous intensity and a combination of surrounding intensities 
@@ -114,26 +120,37 @@ class AreaSimulation:
                    )
                 elif self.grid[(x,y)].fire_inten == 0: # cell is not burning but can catch (assuming cells don't randomly catch fire)
                     ## need to take into account the conditions of surrounding cells
-                    total_inten = 0
+                    total_inten = 0.
                     for dx,dy in self.hood:
                         if (x+dx,y+dy) in self.grid:
                             total_inten += self.grid[(x+dx,y+dy)].fire_inten # will somehow need to handle the surrounding fire  
-                    new_fire_inten = total_inten/8 * self.grid[(x,y)].veg_inten # to do: take into account wind somehow
+                    new_fire_inten = (total_inten * self.grid[(x,y)].veg_inten)/8. # to do: take into account wind somehow
+                    if not self.grid[(x,y)].veg_inten and new_fire_inten:
+                        print x, y, new_fire_inten
                     newgrid[(x,y)] = Cell(
                         x= x,
                         y= y,
                         time= self.grid[(x,y)].time,
-                        veg_inten= self.grid[(x,y)].veg_inten, ## figure out a better way to decay
+                        veg_inten = self.grid[(x,y)].veg_inten,
+                        #veg_vol = max(0.,self.grid[(x,y)].veg_vol - new_fire_inten),
                         wind_direc= self.grid[(x,y)].wind_direc,
                         wind_inten= self.grid[(x,y)].wind_inten,
                         fire_inten= round(new_fire_inten,2),
                         firefighter = False,
                         ff_info = None 
                    )
-        for x,y in new_ff_coord:
-            action = self.grid[(x,y)].ff_info.bestAction() ## somewhere here need to check if it's a legal action
-            newgrid[(x+action[0], y+action[1])].firefighter = True
-            newgrid[(x+action[0], y+action[1])].ff_info = self.grid[(x,y)].ff_info
+                if self.firefighters[x][y]: ## there is a firefighter in that cell
+                    new_ff_coord.append((x, y))
+                    
+        for x, y in new_ff_coord:
+            dx, dy = self.grid[(x,y)].ff_info.bestAction(newgrid) ## somewhere here need to check if it's a legal action
+            newgrid[(x+dx, y+dy)].firefighter = True
+            newgrid[(x+dx, y+dy)].ff_info = self.grid[(x,y)].ff_info
+            newgrid[(x+dx, y+dy)].fire_inten = 0.
+            newgrid[(x+dx, y+dy)].veg_inten = 0.
+            self.firefighters[x+dx][y+dy] = True
+            self.firefighters[x][y] = False
+                    
         self.grid = newgrid
         return newgrid
 
